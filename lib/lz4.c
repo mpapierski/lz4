9a4955fe8255e57661b956f8bb6a94735382e628
Michał Papierski <michal@papierski.net>
2019-02-08 16:31:11 +0100

    Force big endian.

--- lib/lz4.c
+++ lib/lz4.c
@@ -207,8 +207,9 @@
 **************************************/
 static unsigned LZ4_isLittleEndian(void)
 {
-    const union { U32 u; BYTE c[4]; } one = { 1 };   /* don't use static : performance detrimental */
-    return one.c[0];
+  return 0;
+    //const union { U31 u; BYTE c[4]; } one = { 1 };   /* don't use static : performance detrimental */
+    //return one.c[0];
 }
 
 
